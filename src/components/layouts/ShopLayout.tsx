97e73fd20f59c55413e24d2c9194e21c6b7c90f8
wellpinho <wellpinho@outlook.com>
2022-07-03 12:06:35 -0300

    ✨ NEW FEATURE: meta tags and first layout

---
+++ src/components/layouts/ShopLayout.tsx
@@ -0,0 +1,45 @@
+import Head from "next/head";
+
+interface IProps {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+  children?: React.ReactNode;
+}
+
+export const ShopLayout = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}: IProps) => {
+  return (
+    <>
+      <Head>
+        <meta name="description" content={pageDescription} />
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={pageDescription} />
+
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+
+        <title>{title}</title>
+      </Head>
+
+      <nav>{/* navbar */}</nav>
+
+      {/* TODO: sidebar */}
+
+      <main
+        style={{
+          margin: "80px auto",
+          maxWidth: "1440px",
+          padding: "0px 30px",
+        }}
+      >
+        {children}
+      </main>
+
+      <footer>{/* TODO: my custom footer */}</footer>
+    </>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+import { Typography } from "@mui/material";
 import type { NextPage } from "next";
+import { ShopLayout } from "../components/layouts";
 
 const Home: NextPage = () => {
-  return <h1>home</h1>;
+  return (
+    <ShopLayout
+      title={"Teslo-Shop - Home"}
+      pageDescription={"The most amazing products are here"}
+    >
+      <Typography variant="h1">Store</Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All products
+      </Typography>
+    </ShopLayout>
+  );
 };
 
 export default Home;
